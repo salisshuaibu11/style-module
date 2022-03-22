acfdbb82bb3fc286a4dd9e724d1737b3ff523c7a
Salisu Shuaibu <salisshuaibu11@gmail.com>
2022-03-22 11:05:30 +0100

    Initial commit

---
+++ src/component/header/header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from "./style.module.css";
+
+function header() {
+  return (
+    <div className={styles.header}>header</div>
+  )
+}
+
+export default header
